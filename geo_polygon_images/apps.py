d1cca1a57fa8f1005773d9200fbad57e81e713fa
Ivan Proskurnytskyi <prsky.ivan@gmail.com>
2024-03-04 20:51:16 +0100

    Refactored with black

--- geo_polygon_images/apps.py
+++ geo_polygon_images/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class GeoPolygonImagesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'geo_polygon_images'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "geo_polygon_images"
